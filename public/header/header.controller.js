bdfd1115cac5125c3aa0ce58daf7627ced9bef10
Tian Xia <xia.tian@husky.neu.edu>
2015-12-07 13:33:31 -0500

    merge ashish's first commit and add mobile frindlyness

--- public/header/header.controller.js
+++ public/header/header.controller.js
@@ -5,12 +5,17 @@
 
     function HeaderController($scope, $rootScope, $location){
         $scope.$location = $location;
+        $scope.logout = logout;
 
-        if($rootScope.currentUser != null){
+        if($rootScope.currentUser){
             $scope.userName = $rootScope.currentUser.userName;
         }else{
             $scope.userName = "UserName"
         }
-    }
 
+        function logout(){
+            $rootScope.currentUser = null;
+            console.log(currentUser);
+        }
+    }
 })();
